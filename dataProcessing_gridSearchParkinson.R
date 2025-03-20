3a0f45382c249a5156e7b1ff0504f079476a8c43
bmeder <bmeder@gmail.com>
2025-03-20 15:16:49 +0100

    updated code to include 2nd batch of participants

--- dataProcessing_gridSearchParkinson.R
+++ dataProcessing_gridSearchParkinson.R
@@ -6,13 +6,13 @@ library('plyr')
 library('tidyverse')
 
 # set working directory
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 #setwd("/Users/meder/Library/CloudStorage/OneDrive-HMUHealthandMedicalUniversityPotsdam/_Projekte/gridsearch Parkinson/analysis/")
 
-
+#############################################################################################################################
 # data import function to convert .json to .csv ---------------------------
+#############################################################################################################################
 
-# dataImport_Park <- function(dataFile ="data/445.JSON", writecsv = TRUE){
 dataImport_Park <- function(dataFile, writecsv = FALSE){
   #read in json
   myjson <- fromJSON(readLines(dataFile))
@@ -93,19 +93,8 @@ dataImport_Park <- function(dataFile, writecsv = FALSE){
 }
 
 
-cohensd.ci <- function(d, n1, n2, ci = 0.95) {
-  t <- d * sqrt((n1 * n2)/(n1 + n2))
-  capture.output(
-    fit <- compute.es::tes(t = t, n.1 = n1, n.2 = n2, level = 100 * ci),
-    file = "NUL"
-  )
-  c(lower.ci = fit$l.d, upper.ci = fit$u.d)
-}
-
-
-
 # get names of individual data files stored in .JSON format 
-json_files <- list.files(path = "data", pattern = "(?i)\\.json$", full.names = F )
+json_files <- list.files(path = "data/raw_data/all", pattern = "(?i)\\.json$", full.names = F )
 
 # convert .JSON files in .csv files
 # generates one file for behavioral data
@@ -113,7 +102,7 @@ list_of_dfs <- list()
 
 for (i in seq_along(json_files)) {
   
-  dataFile <- paste0("data/", json_files[i])
+  dataFile <- paste0("data/raw_data/all/", json_files[i])
   csv_file <- paste0(dataFile, ".csv")
   
   df <- dataImport_Park(dataFile) # get data
@@ -127,7 +116,87 @@ for (i in seq_along(json_files)) {
 }
 
 results <- bind_rows(list_of_dfs)
-write.csv(results, "data/results.csv", row.names = FALSE)
+write.csv(results, "data/data_gridsearch_Parkinson.csv", row.names = FALSE)
+
+
+
+cohensd.ci <- function(d, n1, n2, ci = 0.95) {
+  t <- d * sqrt((n1 * n2)/(n1 + n2))
+  capture.output(
+    fit <- compute.es::tes(t = t, n.1 = n1, n.2 = n2, level = 100 * ci),
+    file = "NUL"
+  )
+  c(lower.ci = fit$l.d, upper.ci = fit$u.d)
+}
+
+#############################################################################################################################
+# read bonus round data from json and write to .csv --------------------------------------------------
+#############################################################################################################################
+
+dataFile <- "data/all_data_gridsearch_parkinson.json"
+
+#read in json
+myjson    <-fromJSON(dataFile)
+
+# get environments
+smoothEnvironments  <- lapply(fromJSON("data/kernelSmooth.json"), FUN=function(x) matrix(as.numeric(unlist(x)), ncol=3, byrow=TRUE, dimnames=list(seq(1,64),  c('y', 'z', 'x'))))
+
+
+
+# df for bonus round data  
+df_bonus_round <- data.frame()
+
+#loop through json to create data frame
+for (i in 1:nrow(myjson)){
+  
+  subd <- myjson[i,]
+  
+  # get subject data
+  df_subject <- data.frame(id = as.numeric(rep(subd$participantId, 5)),
+                           bonus_env_number = unlist(subd$envOrder)[length(unlist(subd$envOrder))]) %>% 
+    mutate(bonus_environment = "Smooth")
+  
+  #get bonus round environment of current subject
+  subject_bonus_env_number <- df_subject$bonus_env_number[1]
+  
+  bonus_environment <- data.frame(eval(parse(text=paste0("smoothEnvironments$`", subject_bonus_env_number, "`"))))
+  
+  #bonus_environment <- data.frame(Reduce(rbind, bonus_environment))
+  # somehow the column labels get screwed up when extracting the matrix from the list
+  # x1 should be x2; x2 should be y, y should be X2 (see original .json file)
+  #colnames(bonus_environment) <- c('x2', 'y', 'x1') # this is the json file nomenclature
+  #colnames(bonus_environment) <- c('y', 'z', 'x') # this is the "standard" nomenclature
+  
+  # compute true (observed) values on grid
+  scale_factor        <- unlist(subd$scale)[length(unlist(subd$scale))]
+  bonus_environment$z <- bonus_environment$z * scale_factor + 5
+  #bonus_environment$z <- (bonus_environment$z + 0.5) * 50
+  
+  # get bonus round data
+  # reward estimates for five unseen tiles and subjective confidence of reward
+  df_bonus          <- data.frame(subd$bonusLevel$bonusCells)
+  
+  # eventually chose tile
+  df_bonus$chosen_x <- rep(unlist(subd$bonusLevel$finalChosenCell$x),5)
+  df_bonus$chosen_y <- rep(unlist(subd$bonusLevel$finalChosenCell$y),5)
+  
+  # get true value of tiles in bonus round
+  df_bonus$true_z <- NA
+  for(i in 1:5){
+    df_bonus$true_z[i] <- bonus_environment$z[bonus_environment$x == df_bonus$x[i] & bonus_environment$y == df_bonus$y[i]]
+  }
+  
+  # bind
+  df_current <- cbind(df_subject, df_bonus)
+  
+  # put together
+  df_bonus_round <- rbind(df_bonus_round, df_current)
+  
+  
+}
+
+write.table(df_bonus_round, file="data/data_gridsearch_Parkinson_bonusround.csv", sep=",", row.names = F)
+return(df_bonus_round)
 
 
 #############################################################################################################################
--- merge_json_files.R
+++ merge_json_files.R
@@ -1,7 +1,9 @@
 library(jsonlite)
 # List all the JSON files in the folder
-json_files <- list.files(path = "raw_data/json", pattern = "*.JSON", full.names = TRUE)
+json_files_1st_batch <- list.files(path = "data/raw_data/1st batch", pattern = "*.JSON", full.names = TRUE)
+json_files_2nd_batch <- list.files(path = "data/raw_data/2nd batch", pattern = "*.JSON", full.names = TRUE)
 
+json_files <- c(json_files_1st_batch, json_files_2nd_batch)
 # Create an empty list to hold the content of all JSON files
 all_json_content <- list()
 
@@ -12,5 +14,5 @@ for (file in json_files) {
 }
 
 # Write the combined content to a single JSON file as an array
-write_json(all_json_content, "data/all_data_gridsearch_parkinson.json", pretty = TRUE)
+write_json(all_json_content, "data/raw_data/all_data_gridsearch_parkinson.json", pretty = TRUE)
 
