b12daa3d2f810ae09182f2cdaade39f3cefed3cc
bmeder <bmeder@gmail.com>
2025-09-24 15:38:48 +0200

    revising model simulations (parallelized) and plots thereof

--- simulateModels.R
+++ simulateModels.R
@@ -9,19 +9,19 @@ rm(list=ls())
 source("models.R")
 
 #packages
-packages <- c('plyr', 'dplyr', 'jsonlite', 'lsr', 'BayesFactor', 'matrixcalc', 'future.apply', 'data.table', 'progressr')
+packages <- c('plyr', 'dplyr', 'jsonlite', 'lsr', 'BayesFactor', 'matrixcalc', 'furrr', 'data.table')
 lapply(packages, require, character.only = TRUE) #loads packages
 
-# for parallel on local machine
-# handlers(global = TRUE)
-# plan(multisession, workers = parallel::detectCores() -1)  
 set.seed(0511)
-# RNGkind("L'Ecuyer-CMRG")                 
 
+##########################################################################################
+# get environments --------------------------------------------------------
+##########################################################################################
 
 #extract environments
 environments <- lapply(fromJSON("data/kernelRough.json"), FUN=function(x) matrix(as.numeric(unlist(x)), ncol=3, byrow=TRUE, dimnames=list(seq(1,64),  c('x2', 'y', 'x1'))))
 
+# make df
 env <- as.data.frame(environments[[1]])
 for (i in 2:40){
   env<-rbind(env,as.data.frame(environments[[i]]))
@@ -29,17 +29,15 @@ for (i in 2:40){
 env$en<-rep(1:40, each=64)
 
 #Scaling reward range
-env$y<-env$y*50
+env$y <- env$y*50
 
 # simulation rounds for each parameter combination
-replications = 100
-# replications = 2
-
+replications <- 100
 
 #############################################################################################################################
 # Import fitted parameters
 #############################################################################################################################
-# get parameter estimates
+# get corr-validated parameter estimates
 modelFit = read.csv('modelResults/modelFit.csv')
 
 modelFit = modelFit %>%
@@ -52,133 +50,134 @@ modelFit = subset(modelFit, kernel=='GP' & acq=='UCB')
 #############################################################################################################################
 # Simulating performance for different parameter values
 #############################################################################################################################
-# Tukey's fence to compute upper and lower bound for each parameter
-tukeysFence <- function(x, k = 1.5, na.rm = TRUE) {
-  quar <- quantile(x, probs = c(0.25, 0.75), na.rm = na.rm)
-  iqr <- diff(quar)
-  
-  lower = quar[1] - k * iqr
-  upper = quar[2] + k * iqr
-  
-  return(c(lower, upper))
-}
 
-# Use log-ranges
-# lambda_range <- tukeysFence(log(modelFit$lambda))
-# beta_range   <- tukeysFence(log(modelFit$beta))
-# tau_range    <- tukeysFence(log(modelFit$tau)); tau_range[1] <- -5  # enforce lower bound
+# fixed theoretical range
+lambda_vals <- 1 # level of generalization (length-scale of RBF kernel), gfixed to 1.0 or 0.5
 
+# bounds from parameter fitting
+lower_bound <- exp(-5)  # ~0.0067
+upper_bound <- exp(4)   # ~54.598
 
-# fixed theoratical range
-lambda_vals <- 0.5
-beta_vals   <- exp(seq(log(0.001), log(50), length.out = 100))
-tau_vals    <- exp(seq(log(0.001), log(20), length.out = 100))
+# parameter values for tau and beta
+beta_vals <- exp(seq(log(lower_bound), log(upper_bound), length.out = 100))
+tau_vals  <- exp(seq(log(lower_bound), log(upper_bound), length.out = 100))
 
-# lambda_vals <- exp(seq(lambda_range[1], lambda_range[2], length.out = 5))
-# beta_vals   <- exp(seq(beta_range[1],   beta_range[2],   length.out = 20))
-# tau_vals    <- exp(seq(tau_range[1],    tau_range[2],    length.out = 20))
+# beta_vals   <- exp(seq(log(0.001), log(50), length.out = 100))
+# tau_vals    <- exp(seq(log(0.001), log(20), length.out = 100))
 
 params <- expand.grid(lambda = lambda_vals, beta = beta_vals, tau = tau_vals)
 
-# --- small-scale settings ---
-#set.seed(1)
-replications <- 100
-env_ids <- sample(unique(env$en), size = min(5, length(unique(env$en))), replace = FALSE)
 
-dparams <- data.frame(lambda = numeric(), beta = numeric(), tau = numeric(),
-                      mu = numeric(), replication = integer(), stringsAsFactors = FALSE)
-
+# make grid locations
 Xstar <- cbind(env$x1[1:64], env$x2[1:64])
 k<-rbf
 
-param_ids <- seq_len(nrow(params))
+###########################################################################
+# parallelized version (via furrr package) --------------------------------
+###########################################################################
 
-# with_progress({
-#   p <- progressor(steps = length(param_ids))
-#   
-#   res_list <- future_lapply(param_ids, function(i) {
-#     lambda <- params$lambda[i]
-#     beta   <- params$beta[i]
-#     tau    <- params$tau[i]
-#     parVec <- c(lambda, lambda, 1, 1e-4)
-#     
-#     rows <- vector("list", replications)
-#     for (rep_idx in seq_len(replications)) {
-#       enselect <- sample(env_ids, 1)
-#       environ  <- subset(env, en == enselect)
-#       X <- cbind(environ$x1[sample(1:64, 1)], environ$x2[sample(1:64, 1)])
-#       y <- as.matrix(environ$y[sample(1:64, 1)])
-#       
-#       for (trial in 1:25) {
-#         out <- gpr(X.test = Xstar, theta = parVec, X = X, Y = (y - 25)/50, k = k)
-#         util <- ucb(out, beta)
-#         p_vec <- exp((util - max(util)) / tau)
-#         p_vec <- p_vec / sum(p_vec)
-#         p_vec <- pmax(pmin(p_vec, 0.99999), 1e-5)
-#         
-#         ind <- sample(1:64, 1, prob = p_vec)
-#         X   <- rbind(X, cbind(environ$x1[ind], environ$x2[ind]))
-#         y   <- rbind(y, as.matrix(environ$y[ind]))
-#       }
-#       y <- y[-1]
-#       rows[[rep_idx]] <- list(lambda=lambda, beta=beta, tau=tau,
-#                               mu=mean(y), replication=rep_idx)
-#     }
-#     
-#     p()  # increment progress bar
-#     data.table::rbindlist(rows)
-#   })
-# })
-# 
-# dparams <- data.table::rbindlist(res_list)
+# set up parallel plan
+plan(multisession, workers = parallel::detectCores() - 1)
 
-# not parallel
-for (i in seq_len(nrow(params))) {
-
-  lambda <- params$lambda[i]
-  beta   <- params$beta[i]
-  tau    <- params$tau[i]
+# run simulations in parallel
+dparams <- future_pmap_dfr(params, function(lambda, beta, tau) {
+  
   parVec <- c(lambda, lambda, 1, 1e-4)
-
-
+  reps_out <- vector("list", replications)
+  
   for (rep in seq_len(replications)) {
-
-    message(paste(
-      "Param set", i, "/", nrow(params),
-      "lambda=", signif(lambda, 3),
-      "beta=", signif(beta, 3),
-      "tau=", signif(tau, 3),
-      "rep", rep, "/", replications
-    ))
-
-    enselect <- sample(env_ids, 1)
-    environ  <- subset(env, en == enselect)
-
+    enselect <- sample(1:40, 1)
+    environ  <- subset(env, en == enselect)  # unchanged
+    
     ind <- sample(1:64, 1)
     X   <- cbind(environ$x1[ind], environ$x2[ind])
     y   <- as.matrix(environ$y[ind])
-
+    
     for (trial in 1:25) {
       out <- gpr(X.test = Xstar, theta = parVec, X = X, Y = (y - 25)/50, k = k)
       utilityVec <- ucb(out, beta)
-
+      
       utilities <- utilityVec - max(utilityVec)
       p <- exp(utilities / tau)
       p <- p / colSums(p)
       p <- pmax(pmin(p, 0.99999), 1e-5)
-
+      
       ind <- sample(1:64, 1, prob = p)
       X   <- rbind(X, cbind(environ$x1[ind], environ$x2[ind]))
       y   <- rbind(y, as.matrix(environ$y[ind]))
     }
+    
     y <- y[-1]  # drop initial random observation
-    dparams <- rbind(dparams, data.frame(lambda=lambda, beta=beta, tau=tau,
-                                         mu=mean(y), replication=rep))
+    reps_out[[rep]] <- data.frame(lambda=lambda, beta=beta, tau=tau,
+                                  mu=mean(y), replication=rep)
   }
+  
+  do.call(rbind, reps_out)
+}, .progress = TRUE)  # progress bar
+
+
+###########################################################################
+# strictly sequential (loop) version --------------------------------------
+###########################################################################
+# 
+# dparams <- data.frame(lambda = numeric(), beta = numeric(), tau = numeric(),
+#                       mu = numeric(), replication = integer(), stringsAsFactors = FALSE)
+# 
+# for (i in seq_len(nrow(params))) {
+# 
+#   lambda <- params$lambda[i]
+#   beta   <- params$beta[i]
+#   tau    <- params$tau[i]
+#   parVec <- c(lambda, lambda, 1, 1e-4)
+# 
+# 
+#   for (rep in seq_len(replications)) {
+#     enselect<-sample(1:40, 1)
+#     environ  <- subset(env, en == enselect)
+#     
+#     message(paste(
+#       "Param set", i, "/", nrow(params),
+#       "lambda=", signif(lambda, 3),
+#       "beta=", signif(beta, 3),
+#       "tau=", signif(tau, 3),
+#       "env=", enselect,
+#       "rep", rep, "/", replications
+#     ))
+# 
+#     ind <- sample(1:64, 1)
+#     X   <- cbind(environ$x1[ind], environ$x2[ind])
+#     y   <- as.matrix(environ$y[ind])
+# 
+#     for (trial in 1:25) {
+#       out <- gpr(X.test = Xstar, theta = parVec, X = X, Y = (y - 25)/50, k = k)
+#       utilityVec <- ucb(out, beta)
+# 
+#       utilities <- utilityVec - max(utilityVec)
+#       p <- exp(utilities / tau)
+#       p <- p / colSums(p)
+#       p <- pmax(pmin(p, 0.99999), 1e-5)
+# 
+#       ind <- sample(1:64, 1, prob = p)
+#       X   <- rbind(X, cbind(environ$x1[ind], environ$x2[ind]))
+#       y   <- rbind(y, as.matrix(environ$y[ind]))
+#     }
+#     y <- y[-1]  # drop initial random observation
+#     dparams <- rbind(dparams, data.frame(lambda=lambda, beta=beta, tau=tau,
+#                                          mu=mean(y), replication=rep))
+#   }
+# }
+# 
+
+
+###########################################################################
+# write results -----------------------------------------------------------
+###########################################################################
+
+if(lambda_vals == 1) {
+  write.csv(dparams, "modelResults/simulatedModels_local_lambda_1_0.csv", row.names = FALSE)
+} else {
+  write.csv(dparams, "modelResults/simulatedModels_local_lambda_0_5.csv", row.names = FALSE)
 }
-
-write.csv(dparams, "modelResults/simulatedModels_local_lambda_0_5.csv", row.names = FALSE)
-
 
 ##########################################################
 # Original Code from Giron et al 2023 (optimized for cluster)
@@ -190,9 +189,15 @@ write.csv(dparams, "modelResults/simulatedModels_local_lambda_0_5.csv", row.name
 # beta = tukeysFence(log(modelFit$beta))
 # tau = tukeysFence(log(modelFit$tau))
 # 
+
+# Use log-ranges based on Tukey fence
+# lambda_range <- tukeysFence(log(modelFit$lambda))
+# beta_range   <- tukeysFence(log(modelFit$beta))
+# tau_range    <- tukeysFence(log(modelFit$tau)); tau_range[1] <- -5  # enforce lower bound
+
 # # lower bound for tau below range defined for model fitting - set to lower bound
 # tau[1] = -5
-# 
+
 # # parameters to simulate
 # params = expand.grid(lambda=exp(seq(lambda[1], lambda[2], len=100)), beta=exp(seq(beta[1], beta[2], len=100)),
 #                      tau=exp(seq(tau[1], tau[2], len=100)))
@@ -267,3 +272,4 @@ write.csv(dparams, "modelResults/simulatedModels_local_lambda_0_5.csv", row.name
 # 
 # filename = paste0("modelResults/simulatedModels/batch1/simulatedModels_", clusterid, ".csv")
 # write.csv(dparams, filename)
+
